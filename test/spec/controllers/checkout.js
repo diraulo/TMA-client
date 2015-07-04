052b0bf99952a47e5571334166481732d6d56c90
Raoul DIFFOUO <diraulo@gmail.com>
2015-07-04 23:23:28 +0200

    WIP: checkout controller spec

--- test/spec/controllers/checkout.js
+++ test/spec/controllers/checkout.js
@@ -6,13 +6,50 @@ describe('Controller: CheckoutController', function () {
   beforeEach(module('tmaClientApp'));
 
   var CheckoutController,
+    location,
+    rootScope,
+    $window,
+    mockAuthService,
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $location, _$window_) {
+    $window = _$window_;
+    $window.sessionStorage = { // mocking sessionStorage
+        token: function(key) {
+          return this[key];
+        }
+    };
+
+    rootScope = $rootScope;
     scope = $rootScope.$new();
+    location = $location;
+    mockAuthService = {
+      isLoggedIn: function() {
+      },
+      register: function(user) {
+      }
+    };
+
     CheckoutController = $controller('CheckoutController', {
       $scope: scope
     });
   }));
+
+  it('should redirect to order confirmation view if user already logged in', function() {
+    $window.sessionStorage.token = 'token';
+    rootScope.$apply();
+    console.log(location.path());
+    // expect(location.path()).toBe('/confirm-order');
+  });
+
+  describe('$scope.register', function() {
+    it('should create a new user and redirects to order confirmation', function() {
+      var user = { name: 'John', email: 'john@doe.com', password: 'password', password_confirmation: 'password' };
+      rootScope.$apply();
+      spyOn(mockAuthService, 'register');
+      // scope.register(user);
+      // expect(mockAuthService.register).toHaveBeenCalled();
+    });
+  });
 });
